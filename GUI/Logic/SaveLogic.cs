761f62fb67fb69995ce5b2e732f646e4e8724a94
WJ1241 <83521037+WJ1241@users.noreply.github.com>
2022-04-25 17:34:45 +0100

    (WJ) 17:34 - Edited SaveLogic

--- GUI/Logic/SaveLogic.cs
+++ GUI/Logic/SaveLogic.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Windows.Forms;
 using Server.Exceptions;
 using GUI.Logic.Interfaces;
+using System;
 
 namespace GUI.Logic
 {
@@ -34,44 +35,58 @@ namespace GUI.Logic
         /// <param name="pImage"> Image currently displayed on screen </param>
         public void SaveImage(Image pImage)
         {
-            // IF pImage DOES HAVE an active instance:
-            if (pImage != null)
+            // TRY catching exceptions from saving images:
+            try
             {
-                // DECLARE & INSTANTIATE a new SaveFileDialog, name it 'pSFD':
-                using (SaveFileDialog pSFD = new SaveFileDialog())
+                // IF pImage DOES HAVE an active instance:
+                if (pImage != null)
                 {
-                    // SET InitialDirectory to FishAssets folder:
-                    pSFD.InitialDirectory = Path.GetFullPath("..\\..\\..\\..\\Server\\Displayables\\FishAssets");
-
-                    // SET title of Dialog to 'Save Your Image!'
-                    pSFD.Title = "Save Your Image!";
-
-                    // FILTER File Extensions to .PNG:
-                    pSFD.Filter = "PNG Files (*.png)|*.png";
-
-                    // IF User has clicked 'OK' on OpenFileDialog:
-                    if (pSFD.ShowDialog() == DialogResult.OK)
+                    // DECLARE & INSTANTIATE a new SaveFileDialog, name it 'pSFD':
+                    using (SaveFileDialog pSFD = new SaveFileDialog())
                     {
-                        // IF FileName IS NOT empty:
-                        if (pSFD.FileName != "")
+                        // SET InitialDirectory to FishAssets folder:
+                        pSFD.InitialDirectory = Path.GetFullPath("..\\..\\..\\..\\Server\\Displayables\\FishAssets");
+
+                        // SET title of Dialog to 'Save Your Image!'
+                        pSFD.Title = "Save Your Image!";
+
+                        // FILTER File Extensions to .PNG:
+                        pSFD.Filter = "PNG Files (*.png)|*.png";
+
+                        // IF User has clicked 'OK' on OpenFileDialog:
+                        if (pSFD.ShowDialog() == DialogResult.OK)
                         {
-                            // DECLARE & ASSIGN a FileStream, name it 'fs', and give result value of pSFD.OpenFile():
-                            FileStream fs = (FileStream)pSFD.OpenFile();
+                            // IF FileName IS NOT empty:
+                            if (pSFD.FileName != "")
+                            {
+                                // DECLARE & ASSIGN a FileStream, name it 'fs', and give result value of pSFD.OpenFile():
+                                FileStream fs = (FileStream)pSFD.OpenFile();
 
-                            // SAVE pImage to the User's device at chosen directory:
-                            pImage.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                                // SAVE pImage to the User's device at chosen directory:
+                                pImage.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
 
-                            // CLOSE FileStream:
-                            fs.Close();
+                                // CLOSE FileStream:
+                                fs.Close();
+                            }
                         }
                     }
                 }
+                // IF pImage DOES NOT HAVE an active instance:
+                else
+                {
+                    // THROW new FileNotSavedException, with corresponding message:
+                    throw new FileNotSavedException("ERROR: Requested Image to be saved does not exist in Program!");
+                }
             }
-            // IF pImage DOES NOT HAVE an active instance:
-            else
+            // CATCH IOException from SaveFileDialog.OpenFile():
+            // THIS IS CAUGHT BECAUSE AN IMAGE BEING USED WILL BECOME NULL AND THEREFORE STORAGE WILL THEN CONTAIN A NULL IMAGE
+            catch (IOException pException)
             {
-                // THROW new FileNotSavedException, with corresponding message:
-                throw new FileNotSavedException("ERROR: Requested Image to be saved does not exist in Program!");
+                // WRITE exception message to console:
+                Console.WriteLine(pException.Message);
+
+                // SHOW Error message to detail that user cannot overwrite an image that is in use:
+                MessageBox.Show("Images currently being stored in the app cannot be overwritten!", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
